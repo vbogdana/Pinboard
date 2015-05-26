4217a3b7621cca6ece6104fd3fb05c0ef5c83b60
vbogdana <bogdana.veselinovic@yahoo.com>
2015-05-26 16:36:53 +0200

    CodeIgniter dodat 1

---
+++ implementation/CodeIgniter/application/views/home/signUp.php
@@ -0,0 +1,46 @@
+<!-- autor: Luka Jovanovic-->
+<html>
+	<head>
+	<link rel="stylesheet" type="text/css" href="css/css_index/style.css">
+	<link href='http://fonts.googleapis.com/css?family=Shadows+Into+Light' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
+		<title>
+		PinBoard - Sign up
+		</title>
+		<style>
+		::-webkit-input-placeholder {
+			color: rgba(255,255,255,0.6);
+		}
+
+		::-moz-input-placeholder {
+			color: rgba(255,255,255,0.6);
+		}
+		</style>
+	</head>
+	
+	<body class="Body">
+	<div id="bg">
+		<img src="images/photos/wallpaper2.jpg" width="100%" height="100%">
+	</div>
+		<div class="Slide">
+			<div class="SlideTitle">
+			welcome to PinBoard!
+			</div>
+			
+				<div class="signup1">
+					<input type="text" placeholder="username" name="user">
+					<input type="password" placeholder="password" name="password">
+					
+					
+				</div>
+				<div class="signup">
+				<input type="password" placeholder="re-password" name="re-password" > 
+					<input type="text" placeholder="e-mail" name="e-mail">
+					<a href="boardSignUp.html">
+						<input type="button" value="sign up" id="btn_click">
+					</a>
+				</div>
+		</div>
+		
+	</body>
+</html>
---
+++ implementation/CodeIgniter/application/views/templates/header.php
@@ -0,0 +1,16 @@
+
+
+<!DOCTYPE html>
+<html lang="en-US">
+
+	<?php $this->load->view('templates/include'); ?>	
+	
+	<!-- Title of the page -->
+	<head>
+		<title>	Pinboard - <?php echo $username ?> </title>
+	</head>
+	
+	<body onload="javascript:fg_hideform('fg_formContainer','fg_backgroundpopup');">
+		
+		<!-- Container -->
+		<div class="Container">
